1f14aee6c7cf8416d5361020abb9ab24d8d1bcea
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2019-12-20 21:09:47 +0530

    user Defined module to calculate checksum
    this is user define module contain function to calculate the checksum of file

---
+++ ChecksumModulle.py
@@ -0,0 +1,11 @@
+import hashlib
+def CheckSum(path,blocksize=1024):
+    fd=open(path,'rb')
+    hasher=hashlib.md5()
+    buf=fd.read(blocksize)
+
+    while len(buf)>0:
+        hasher.update(buf)
+        buf=fd.read(blocksize)
+    fd.close()
+    return hasher.hexdigest()    
